ba8fb3759f4a082cf496bc400eb55b075611ef86
alvisjohnmark <johnmarkalvis02@gmail.com>
2025-02-18 10:25:18 +0800

    .

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
 import ytdl from "@distube/ytdl-core";
-import dotenv from "dotenv";
-
-dotenv.config();
+import fs from "fs";
 
 const app = express();
 app.use(cors());
+
+const cookies = JSON.parse(fs.readFileSync("cookies.json", "utf8"));
 
 app.get("/download", async (req, res) => {
   let url = req.query.url;
@@ -19,8 +19,6 @@ app.get("/download", async (req, res) => {
     return res.status(400).json({ error: "Invalid YouTube URL" });
   }
 
-  console.log("Using cookies:", process.env.YT_COOKIES); // Debugging cookies
-
   res.header("Content-Disposition", 'attachment; filename="audio.mp3"');
 
   try {
@@ -29,10 +27,9 @@ app.get("/download", async (req, res) => {
       quality: "highestaudio",
       requestOptions: {
         headers: {
-          cookie: process.env.YT_COOKIES, 
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-          "Accept-Language": "en-US,en;q=0.9",
+          Cookie: Object.entries(cookies)
+            .map(([key, value]) => `${key}=${value}`)
+            .join("; "),
         },
       },
     };
@@ -40,7 +37,7 @@ app.get("/download", async (req, res) => {
     ytdl(url, options).pipe(res);
   } catch (error) {
     console.error("Download error:", error);
-    res.status(500).json({ error: "Failed to download audio" });
+    res.status(500).json({ error: "Failed to download" });
   }
 });
 
